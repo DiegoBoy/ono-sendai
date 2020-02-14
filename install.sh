069e3ff6e969f8e0896b38fb0e2809fc5504b74e
db <>
2020-02-14 14:37:24 -0800

    Install VS Code

--- install.sh
+++ install.sh
@@ -57,6 +57,23 @@ echo "[X] Terminal"
 
 
 
+### Code editor (vs code)
+
+# install apt over https
+sudo apt-get install apt-transport-https
+
+# add vs code to sources
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+# install vs code
+sudo apt-get update
+sudo apt-get install code
+
+echo "[X] Code editor"
+
+
+
 # go hack stuff
-echo "
-ono-sendai env installed, log out and log in again, use terminator and go hack stuff x_x"
+echo "ono-sendai env installed, log out and log in again, use terminator and go hack stuff x_x"
