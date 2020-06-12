beaeead6d917669e999c956e2123faee3c2b8ae3
db <>
2020-06-11 18:27:26 -0700

    Install bat, a cat replacement with syntax highlighting

--- install.sh
+++ install.sh
@@ -5,10 +5,17 @@ echo "Installing..."
 
 
 
-### Shell (oh-my-zsh + plugins + theme PowerLevel9k)
+### Shell (terminator + zsh + oh-my-zsh + plugins + PowerLevel9k theme + hack font + powerline icons)
 
-# install zsh shell and powerline icons
-sudo apt-get install zsh fonts-powerline -y
+# install terminator + zsh + hack font + powerline icons
+sudo apt-get install terminator zsh fonts-hack-ttf fonts-powerline -y
+
+# config terminator
+mkdir -p ~/.config/terminator/
+cp terminator.config ~/.config/terminator/config
+sudo sed -i 's/terminal.vte.feed_child(command, len(command))/terminal.vte.feed_child(command.encode("utf-8"))/g' /usr/share/terminator/terminatorlib/plugins/custom_commands.py 
+sudo py3clean /usr/share/terminator/terminatorlib/plugins
+sudo python -m compileall /usr/share/terminator/terminatorlib/plugins
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -22,56 +29,35 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/
 # install autosuggestion plugin for oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-# install grc general colourizer and clean up
-git clone https://github.com/garabik/grc.git
-cd grc
-sudo ./install.sh
-cd ..
-rm -Rf grc
-
 # update zsh config file
 cp .zshrc ~/.zshrc
 
 # switch default shell to zsh
 chsh -s $(which zsh)
 
-echo "[X] Shell"
 
 
+### Utilities
 
-### Terminal (terminator)
+# install bat (cat replacement with syntax-highlighting) and clean up
+wget --content-disposition https://github.com/sharkdp/bat/releases/download/v0.13.0/bat_0.13.0_amd64.deb
+sudo dpkg -i bat_0.13.0_amd64.deb
+rm bat_0.13.0_amd64.deb
 
-# install terminator
-sudo apt-get install terminator -y
+# install grc (general colourizer) and clean up
+git clone https://github.com/garabik/grc.git
+cd grc
+sudo ./install.sh
+cd ..
+rm -Rf grc
 
-# update terminator config file
-mkdir -p ~/.config/terminator/
-cp terminator.config ~/.config/terminator/config
-
-# fix termiantor custom commands menu plugin
-sudo sed -i 's/terminal.vte.feed_child(command, len(command))/terminal.vte.feed_child(command.encode("utf-8"))/g' /usr/share/terminator/terminatorlib/plugins/custom_commands.py
-sudo py3clean /usr/share/terminator/terminatorlib/plugins
-sudo python -m compileall /usr/share/terminator/terminatorlib/plugins
-
-echo "[X] Terminal"
-
-
-
-### Code editor (vs code)
-
-# install apt over https
+# install vs code
 sudo apt-get install apt-transport-https
-
-# add vs code to sources
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-
-# install vs code
 sudo apt-get update
 sudo apt-get install code
-
-echo "[X] Code editor"
 
 
 
