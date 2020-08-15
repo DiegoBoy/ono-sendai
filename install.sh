25a1261fd4d6a6ddf7b7c36a9b58c262270487e6
db <>
2020-08-14 17:22:51 -0700

    Remove gpg file after adding repo

--- install.sh
+++ install.sh
@@ -66,6 +66,7 @@ sudo apt-get install apt-transport-https
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+rm packages.microsoft.gpg
 
 # install vs code
 sudo apt-get update
