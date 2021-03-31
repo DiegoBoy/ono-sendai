907c018fc1d27b884eca7dab95fb5d2207279e6a
0xdb <>
2021-03-31 08:16:57 -0400

    Cleaned up installer, added plugins and customized top panel

--- .zshrc
+++ .zshrc
@@ -12,59 +12,27 @@ ZSH_THEME="powerlevel9k/powerlevel9k"
 
 POWERLEVEL9K_MODE='nerdfont-complete'
 
-
-# Set list of themes to pick from when loading at random
-# Setting this variable when ZSH_THEME=random will cause zsh to load
-# a theme from this variable instead of looking in ~/.oh-my-zsh/themes/
-# If set to an empty array, this variable will have no effect.
-# ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
-
-# Uncomment the following line to use case-sensitive completion.
-# CASE_SENSITIVE="true"
-
-# Uncomment the following line to use hyphen-insensitive completion.
-# Case-sensitive completion must be off. _ and - will be interchangeable.
-# HYPHEN_INSENSITIVE="true"
-
-# Uncomment the following line to disable bi-weekly auto-update checks.
-# DISABLE_AUTO_UPDATE="true"
-
-# Uncomment the following line to automatically update without prompting.
-# DISABLE_UPDATE_PROMPT="true"
-
-# Uncomment the following line to change how often to auto-update (in days).
-# export UPDATE_ZSH_DAYS=13
-
-# Uncomment the following line if pasting URLs and other text is messed up.
-# DISABLE_MAGIC_FUNCTIONS=true
-
-# Uncomment the following line to disable colors in ls.
-# DISABLE_LS_COLORS="true"
-
-# Uncomment the following line to disable auto-setting terminal title.
-# DISABLE_AUTO_TITLE="true"
-
-# Uncomment the following line to enable command auto-correction.
-# ENABLE_CORRECTION="true"
-
 # Uncomment the following line to display red dots whilst waiting for completion.
 COMPLETION_WAITING_DOTS="true"
 
-# Uncomment the following line if you want to disable marking untracked files
-# under VCS as dirty. This makes repository status check for large repositories
-# much, much faster.
-# DISABLE_UNTRACKED_FILES_DIRTY="true"
+# hide EOL sign ('%')
+export PROMPT_EOL_MARK=""
 
-# Uncomment the following line if you want to change the command execution time
-# stamp shown in the history command output.
-# You can set one of the optional three formats:
-# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-# or set a custom format using the strftime function format specifications,
-# see 'man strftime' for details.
-# HIST_STAMPS="mm/dd/yyyy"
+new_line_before_prompt=yes
+precmd() {
+    # Print the previously configured title
+    print -Pnr -- "$TERM_TITLE"
 
-# Would you like to use another custom folder than $ZSH/custom?
-# ZSH_CUSTOM=/path/to/new-custom-folder
+    # Print a new line before the prompt, but only if it is not the first line
+    if [ "$new_line_before_prompt" = yes ]; then
+	    if [ -z "$_NEW_LINE_BEFORE_PROMPT" ]; then
+	        _NEW_LINE_BEFORE_PROMPT=1
+	    else
+	        print ""
+	    fi
+    fi
+}
+
 
 # Which plugins would you like to load?
 # Standard plugins can be found in ~/.oh-my-zsh/plugins/*
@@ -73,6 +41,7 @@ COMPLETION_WAITING_DOTS="true"
 # Add wisely, as too many plugins slow down shell startup.
 plugins=(
   colored-man-pages
+  command-not-found
   extract
   git
   zsh-autosuggestions
@@ -80,38 +49,20 @@ plugins=(
 )
 
 source $ZSH/oh-my-zsh.sh
-
-# User configuration
-
-# export MANPATH="/usr/local/man:$MANPATH"
-
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
-
-# Compilation flags
-# export ARCHFLAGS="-arch x86_64"
+source /usr/share/doc/fzf/examples/key-bindings.zsh
+source /usr/share/doc/fzf/examples/completion.zsh
 
 # Set personal aliases, overriding those provided by oh-my-zsh libs,
 # plugins, and themes. Aliases can be placed here, though oh-my-zsh
 # users are encouraged to define aliases within the ZSH_CUSTOM folder.
 # For a full list of active aliases, run `alias`.
-
-# include GRC aliases
-[[ -s "/etc/grc.zsh" ]] && source /etc/grc.zsh
-
-#
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
-alias ls='grc ls --color=always'
+alias bat='cat'
 alias l='ls -al'
-alias nmap='grc nmap'
 alias x='grc extract'
+
+# include GRC aliases
+[[ -s "/etc/grc.zsh" ]] && source /etc/grc.zsh
 
--- install.sh
+++ install.sh
@@ -1,67 +1,108 @@
 #!/bin/bash
 # ono-sendai installer
 
-echo "Installing..."
+# update repos first
+sudo apt-get update > /dev/null
 
 
 
-### Shell (terminator + zsh + oh-my-zsh + plugins + PowerLevel9k theme + hack font + powerline icons)
+### Terminal
+echo "[*] Installing terminal..."
 
-# install terminator + zsh + hack font + powerline icons
-sudo apt-get install terminator zsh fonts-hack-ttf fonts-powerline -y
-
-# config terminator
+# install terminator
+sudo apt-get install -y terminator > /dev/null
 mkdir -p ~/.config/terminator/
 cp terminator.config ~/.config/terminator/config
-sudo sed -i 's/terminal.vte.feed_child(command, len(command))/terminal.vte.feed_child(command.encode("utf-8"))/g' /usr/share/terminator/terminatorlib/plugins/custom_commands.py 
-sudo py3clean /usr/share/terminator/terminatorlib/plugins
-sudo python -m compileall /usr/share/terminator/terminatorlib/plugins
+echo "TerminalEmulator=terminator" > ~/.config/xfce4/helpers.rc
+
+
+### Shell
+echo "[*] Installing shell..."
+
+# install zsh, fonts and icons
+sudo apt-get install -y zsh fonts-hack-ttf fonts-powerline > /dev/null
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo 'exit' | sh -c "$(curl -s -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" > /dev/null
 
-# install theme PowerLevel9K for oh-my-zsh
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+# install theme PowerLevel9K
+git clone -q https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-# install syntax highlighting plugin for oh-my-zsh
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-
-# install autosuggestion plugin for oh-my-zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+# install plugins
+sudo apt-get install -y fzf > /dev/null
+git clone -q https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone -q https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 # update zsh config file
 cp .zshrc ~/.zshrc
+sudo cp .zshrc /root/.zshrc
 
 # switch default shell to zsh
-echo "[!] To continue with installation, exit zsh once oh-my-zsh loads"
 chsh -s $(which zsh)
 
 
 
 ### Utilities
+echo "[*] Installing utilities..."
 
 # install bat (cat replacement with syntax-highlighting) and clean up
-wget --content-disposition https://github.com/sharkdp/bat/releases/download/v0.13.0/bat_0.13.0_amd64.deb
-sudo dpkg -i bat_0.13.0_amd64.deb
-rm bat_0.13.0_amd64.deb
+sudo apt-get install -y bat > /dev/null
 
-# install grc (general colourizer) and clean up
-git clone https://github.com/garabik/grc.git
-cd grc
-sudo ./install.sh
-cd ..
-rm -Rf grc
+# install grc (general colourizer)
+sudo apt-get install -y grc > /dev/null
 
 # install vs code
-sudo apt-get install apt-transport-https
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo apt-get install -y apt-transport-https > /dev/null
+curl -s -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 rm packages.microsoft.gpg
-sudo apt-get update
-sudo apt-get install code
+sudo apt-get update > /dev/null
+sudo apt-get install -y code > /dev/null
 
 
 
-# go hack stuff
-echo "ono-sendai env installed, log out and log in again, use terminator and go hack stuff x_x"
+### UX - Panel
+echo "[*] Customizing top panel..."
+
+# cache newest plugin ID
+last_id=$(xfconf-query -c xfce4-panel -p /panels/panel-1/plugin-ids| grep -v "Value is an\|^$" | sort -n | tail -1)
+
+# remove existing launchers
+rm -r ~/.config/xfce4/panel/launcher-*
+xfconf-query -c xfce4-panel -p /plugins/plugin-4 -rR
+xfconf-query -c xfce4-panel -p /plugins/plugin-5 -rR
+xfconf-query -c xfce4-panel -p /plugins/plugin-6 -rR
+xfconf-query -c xfce4-panel -p /plugins/plugin-7 -rR
+xfce4-panel --restart
+
+# create directory menu
+xfconf-query -c xfce4-panel -p /plugins/plugin-4 -t string -s "directorymenu" --create
+xfconf-query -c xfce4-panel -p /plugins/plugin-4/icon-name -t string -s "system-file-manager" -a --create
+xfconf-query -c xfce4-panel -p /plugins/plugin-4/base-directory -t string -s "$HOME" -a --create
+sleep 1
+
+# create launcher (terminal)
+xfce4-panel --add=launcher /usr/share/applications/terminator.desktop
+sleep 1
+
+# create launcher (firefox)
+xfce4-panel --add=launcher /usr/share/applications/firefox-esr.desktop
+sleep 1
+
+# create launcher (burp)
+xfce4-panel --add=launcher /usr/share/applications/kali-burpsuite.desktop
+sleep 1
+
+# apply panel changes
+for i in $(seq 1 $last_id); do all_ids="$all_ids -t int -s $i"; done
+xfconf-query -c xfce4-panel -p /panels/panel-1/plugin-ids -rR
+xfconf-query -c xfce4-panel -p /panels/panel-1/plugin-ids $(echo $all_ids) --create
+xfce4-panel --restart
+
+
+
+### Done
+echo ""
+echo "[*] ono-sendai env installed, use terminator and go hack stuff x_x"
+
